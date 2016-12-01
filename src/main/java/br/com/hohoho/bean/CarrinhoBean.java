6e44b4d64a459d6816ac845df3ca90358b884a46
SAMUEL BRISTOT LOLI <samuel.loli@ifsc.edu.br>
2016-12-01 13:26:28 -0200

    metade da alteração de remoção qtd - Ainda não pronto

--- src/main/java/br/com/hohoho/bean/CarrinhoBean.java
+++ src/main/java/br/com/hohoho/bean/CarrinhoBean.java
@@ -1,11 +1,18 @@
 package br.com.hohoho.bean;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 
+import org.primefaces.component.datatable.DataTable;
+
 import br.com.hohoho.dao.CarrinhoDAO;
+import br.com.hohoho.dao.ItemComercialDAO;
 import br.com.hohoho.modelo.CarrinhoCompra;
+import br.com.hohoho.modelo.ItemComercial;
 
 @ManagedBean
 @ViewScoped
@@ -13,22 +20,56 @@ import br.com.hohoho.modelo.CarrinhoCompra;
 public class CarrinhoBean {
 	FacesContext context = FacesContext.getCurrentInstance();
 	private CarrinhoCompra carrinho = (CarrinhoCompra) context.getExternalContext().getSessionMap()
-			.get("carrinhoCompra");
+			.get("carrinhoCompra");	
+	private Integer quantidadeSelecionada;
+	 private DataTable dataTable;
 
 	public String removerDoCarrinho() {
 		FacesContext context = FacesContext.getCurrentInstance();
 		String idItem = (String) context.getExternalContext().getRequestParameterMap().get("idItem");
-		CarrinhoDAO.getInstance().removeDoCarrinho(Long.valueOf(idItem), this.carrinho);
+		ItemComercial itemRemocao = ItemComercialDAO.getInstance().buscaPorId(Long.valueOf(idItem));
+		if(this.quantidadeSelecionada == itemRemocao.getQuantidade()){
+			CarrinhoDAO.getInstance().removeDoCarrinho(Long.valueOf(idItem), this.carrinho);
+		}
+		else{		
+			//
+		}
+//		ItemComercial itemRemocao = (ItemComercial) dataTable.getRowData();
+//		CarrinhoDAO.getInstance().removeDoCarrinho(itemRemocao.getId(), this.carrinho);
 		return "carrinho?faces-redirect=true";
-
 	}
 
 	public CarrinhoCompra getCarrinho() {
 		return carrinho;
 	}
+	
+	public List<Integer> quantidadeDoItem(){
+		ItemComercial itemQuantidade = (ItemComercial) dataTable.getRowData();
+		List<Integer> quantidadesPossiveisDoItem = new ArrayList<>();
+		for(int i=1;i<=itemQuantidade.getQuantidade();i++){
+			quantidadesPossiveisDoItem.add(i);
+		}
+		return quantidadesPossiveisDoItem;
+	}
 
 	public void setCarrinho(CarrinhoCompra carrinho) {
 		this.carrinho = carrinho;
+	}
+
+	public Integer getQuantidadeSelecionada() {
+		return quantidadeSelecionada;
+	}
+
+	public void setQuantidadeSelecionada(Integer quantidadeSelecionada) {
+		this.quantidadeSelecionada = quantidadeSelecionada;
+	}
+
+	public DataTable getDataTable() {
+		return dataTable;
+	}
+
+	public void setDataTable(DataTable dataTable) {
+		this.dataTable = dataTable;
 	}
 
 }
